70aad6091fbe6f617effb6d2d73e35f018ac61d3
Hongxiong Xian (Wicresoft) <v-hoxian@microsoft.com>
2017-08-15 16:48:45 +0800

    Modify the path of vhd

--- Main.ps1
+++ Main.ps1
@@ -1,4 +1,5 @@
 
 .\PrepareImageForCloudinit.ps1
 
-.\Upload_VHD_FromLocalToAzureStorage_ARMv140.ps1
+$vhdPath = "$pwd" +"\CloudinitBase.vhd"
+.\Upload_VHD_FromLocalToAzureStorage_ARMv140.ps1   -VHDFile $vhdPath
